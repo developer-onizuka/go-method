9841b25b6c18662f5d5be946a26ee8c7fae48299
developer-onizuka <his.onioni@gmail.com>
2020-09-26 10:15:32 +0900

    2020-09-26

--- method/main.go
+++ method/main.go
@@ -7,16 +7,29 @@ import (
 
 type LocalSquare mathlib.Square
 func (s *LocalSquare) Area() float64 {
-	return math.Sqrt((*s).Width * (*s).Height)
+	//return math.Sqrt((*s).Width * (*s).Height)
+	return math.Sqrt(s.Width * s.Height)
 }
 
 func main() {
 	fmt.Printf("----------\n")
 	s := &mathlib.Square{ 1.0, 1.0 }
 	t := &LocalSquare{ 1.0, 1.0 }
-	fmt.Printf("Value is %v and %v\n", *s, *t)
+	fmt.Printf("Value is %v and %v\n", s, t)
+	fmt.Printf("  (*s).Width: %v (*s).Height: %v\n", (*s).Width, (*s).Height)
+	fmt.Printf("  (*t).Width: %v (*t).Height: %v\n", (*t).Width, (*t).Height)
+	fmt.Printf("  s.Width: %v    s.Height: %v\n", s.Width, s.Height)
+	fmt.Printf("  t.Width: %v    t.Height: %v\n", t.Width, t.Height)
 	fmt.Printf("Type is %T and %T\n", s, t)
+	fmt.Printf("  (*s).Width: %T (*s).Height: %T\n", (*s).Width, (*s).Height)
+	fmt.Printf("  (*t).Width: %T (*t).Height: %T\n", (*t).Width, (*t).Height)
+	fmt.Printf("  s.Width: %T    s.Height: %T\n", s.Width, s.Height)
+	fmt.Printf("  t.Width: %T    t.Height: %T\n", t.Width, t.Height)
 	fmt.Printf("Address is %p and %p\n", s, t)
+	fmt.Printf("  (*s).Width: %p (*s).Height: %p\n", &(*s).Width, &(*s).Height)
+	fmt.Printf("  (*t).Width: %p (*t).Height: %p\n", &(*t).Width, &(*t).Height)
+	fmt.Printf("  s.Width: %p    s.Height: %p\n", &s.Width, &s.Height)
+	fmt.Printf("  t.Width: %p    t.Height: %p\n", &t.Width, &t.Height)
 	fmt.Printf("Sqrt is %v\n", (*s).Sqrt())
 	fmt.Printf("Area is %v\n", (*t).Area())
 	fmt.Printf("----------\n")
---
+++ method/pointer.go
@@ -0,0 +1,29 @@
+package main
+import (
+	"fmt"
+	"mathlib"
+	"reflect"
+)
+
+func main() {
+	fmt.Printf("----------\n")
+	var ptr uintptr = reflect.ValueOf(mathlib.Volume).Pointer()
+	fmt.Printf("Volume: 0x%x\n", ptr)
+	var ptrp uintptr = reflect.ValueOf(mathlib.Volumep).Pointer()
+	fmt.Printf("Volumep: 0x%x\n", ptrp)
+	fmt.Printf("----------\n")
+	fmt.Printf("\n")
+	p := mathlib.Xyz{ 1.0, 2.0, 3.0 }
+	fmt.Printf("Address:\n" )
+	fmt.Printf("p.X: %v, %p\n", p.X, &p.X)
+	fmt.Printf("p.Y: %v, %p\n", p.Y, &p.Y)
+	fmt.Printf("p.Z: %v, %p\n", p.Z, &p.Z)
+	fmt.Printf("Volume: %v\n", mathlib.Volume(p))
+	fmt.Printf("\n")
+	pp := &mathlib.Xyz{ 3.0, 4.0, 5.0 }
+	fmt.Printf("Address:\n")
+	fmt.Printf("(*pp).X: %v, %p\n", (*pp).X, &(*pp).X)
+	fmt.Printf("(*pp).Y: %v, %p\n", (*pp).Y, &(*pp).Y)
+	fmt.Printf("(*pp).Z: %v, %p\n", (*pp).Z, &(*pp).Z)
+	fmt.Printf("Volume: %v\n", mathlib.Volumep(pp))
+}
